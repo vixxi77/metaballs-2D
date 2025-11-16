afd811b640f6f6979f74b117139f4537ace6559b
yaye <viktorastankus37@gmail.com>
2025-11-17 00:47:28 +0200

    minor changes

--- metaballs.c
+++ metaballs.c
@@ -15,14 +15,14 @@ typedef struct {
 	int radius;
 } Metaball;
 
-void draw_circle(Metaball *metaball){
+void draw_circle(Metaball *metaball, const Color *_color){
 	int x, y, r2, height;	
 	r2 = metaball->radius*metaball->radius;
 
 	for(x = -metaball->radius; x <= metaball->radius; x++){
 		height = (int)sqrt((double)(r2 - x * x) + 0.5);
 		for(y = -height; y <= height; y++){
-			DrawPixel(metaball->x + x, metaball->y + y, BLUE);	
+			DrawPixel(metaball->x + x, metaball->y + y, *_color);	
 		}
 	}
 }
@@ -48,13 +48,16 @@ int main(){
 	InitWindow(WIDTH, HEIGHT, "Metaballz");
 	SetTargetFPS(60);
 	Metaball metaball = {400, 400, 100};
+	Metaball metaball1 = {200, 200, 100};
 	
 
 	while(!WindowShouldClose()){
 		BeginDrawing();
 		ClearBackground(WHITE);
 		circle_follow_cursor(&metaball);
-		draw_circle(&metaball);
+		draw_circle(&metaball, &BLUE);
+		draw_circle(&metaball1, &RED);
+		DrawFPS(10, 10);
 		EndDrawing();
 	}	
 	CloseWindow();
