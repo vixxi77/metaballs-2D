f382bf580bffa114e8ea52dfea56ddd30c1b093c
yaye <viktorastankus37@gmail.com>
2025-11-26 21:57:49 +0200

    experimental function

--- metaballs.c
+++ metaballs.c
@@ -5,12 +5,6 @@
 #define HEIGHT 800
 
 typedef struct {
-	int r;
-	int g;
-	int b;
-} Pixel;
-
-typedef struct {
 	int x, y;
 	int radius;
 } Metaball;
@@ -29,6 +23,27 @@ draw_circle(Metaball *metaball, const Color *_color){
 }
 
 void 
+draw_metaball(Metaball *metaball, const Color *_color, double _threshold, int _count){
+	int x, y;	
+	for(y = 0; y < HEIGHT; y++){
+		for(x = 0; y < WIDTH; x++){
+			double field = 0.0;
+		
+			for(int i = 0; i < _count; i++){
+				double dx = x - metaball[i].x;
+				double dy = y - metaball[i].y;
+				double r2 = metaball[i].radius * metaball[i].radius;
+				
+				field += r2 / (dx*dx + dy*dy + 1e-6);
+			}
+			if(field > _threshold){
+				DrawPixel(x, y, *_color);
+			}
+		}
+	}
+}
+
+void 
 circle_follow_cursor(Metaball *metaball){
 	metaball->x = GetMousePosition().x;
 	metaball->y = GetMousePosition().y;
@@ -41,7 +56,6 @@ int main(){
 	SetTargetFPS(60);
 	Metaball metaball = {400, 400, 100};
 	Metaball metaball1 = {200, 200, 100};
-	
 
 	while(!WindowShouldClose()){
 		BeginDrawing();
@@ -49,6 +63,7 @@ int main(){
 		circle_follow_cursor(&metaball);
 		draw_circle(&metaball, &BLUE);
 		draw_circle(&metaball1, &RED);
+		//draw_metaball(balls, &RED, 2, 1.0);
 		DrawFPS(10, 10);
 		EndDrawing();
 	}	
